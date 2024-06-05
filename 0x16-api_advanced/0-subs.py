5377750902840f18b9764cb9018a67dcd7d8d0ca
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-06-05 02:47:10 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,9 +13,7 @@ def number_of_subscribers(subreddit):
     for a given subreddit. If the subreddit is invalid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'MyRedditApp/0.1 by YourUsername'
-    }
+    headers = {'User-Agent': 'python:my-reddit-app:v1.0 (by /u/yourusername)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
